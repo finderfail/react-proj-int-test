4f220c4bd8eeafcda95baf03dbf1555756c0a890
Ilya Lvov <ilyalvov2007@gmail.com>
2026-05-04 09:41:57 +0300

    shish

--- my-app-js/src/components/Card/Card.jsx
+++ my-app-js/src/components/Card/Card.jsx
@@ -1,4 +1,4 @@
-import styles from './cards.module.css'
+import styles from './Card.module.css'
 
 export const Card = ({title, categories, desc}) => {
     return (
@@ -6,7 +6,7 @@ export const Card = ({title, categories, desc}) => {
                                 <h3 className={styles.cardTitle}>{title}</h3>
                                 <p className={styles.grayTextLite}>{categories?.map((cat, index) => <span key={index}>{cat}</span>)}</p>
                                 <p className={styles.littleTextHH}>{desc}</p>
-                                <button className={styles.selectedButton} type="button">Пройти заного</button>
+                                <button className={styles.selectedButton} type="button">Пройти заново</button>
                             </article>
     )
 }
