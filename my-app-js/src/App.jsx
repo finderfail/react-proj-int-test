bfc68fc752bd7eeb613b97315d94e7b2f698e452
Ilya Lvov <ilyalvov2007@gmail.com>
2026-05-04 14:52:03 +0300

    RAHHH

--- my-app-js/src/App.jsx
+++ my-app-js/src/App.jsx
@@ -9,6 +9,8 @@ import { Header } from './components/Header/Header.jsx'
 import { ProgressBar } from './components/ProgressBar/ProgressBar.jsx'
 import { MainBlock } from './components/MainBlock/MainBlock.jsx'
 import { Row } from './components/Row/Row.jsx'
+import { Circle } from './components/Circle/Circle.jsx'
+
 
 
 
@@ -24,13 +26,7 @@ function App() {
                           <p className={style.headline}>Рекомендуемые темы</p>
                           <div className={style.whiteBlock}>
                               <div className={style.circle}>
-                                  <svg width="170" height="170" viewBox="0 0 170 170" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                      <path
-                                          d="M85 8C101.261 8 117.104 13.1478 130.259 22.7057C143.415 32.2636 153.206 45.7408 158.231 61.2057C163.256 76.6706 163.256 93.3294 158.231 108.794C153.206 124.259 143.415 137.736 130.259 147.294C117.104 156.852 101.261 162 85 162C68.7392 162 52.8958 156.852 39.7405 147.294C26.5853 137.736 16.7935 124.259 11.7686 108.794C6.74378 93.3294 6.74378 76.6706 11.7687 61.2057"
-                                          stroke="#24C38E"
-                                          strokeWidth="16"
-                                      />
-                                  </svg>
+                                  <Circle />
                                   <ProgressBar />
                               </div>
                               <MainBlock title="Начало работы" categories={["Для новичка", " Основы работы"]} desc="Познакомьтесь ближе с компанией и узнайте больше о том, что вы делаете" prButton="Продолжить тему"  />
