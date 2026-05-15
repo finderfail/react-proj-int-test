bb782d2c59f75eefaf8bfae6278aeaba2d12168b
Ilya Lvov <ilyalvov2007@gmail.com>
2026-05-15 13:10:01 +0300

    kj

--- my-app-js/src/components/Card/Card.jsx
+++ my-app-js/src/components/Card/Card.jsx
@@ -1,12 +1,17 @@
 import styles from './Card.module.css'
 
-export const Card = ({title, categories, desc}) => {
+export const Card = ({title, categories, desc, done}) => {
     return (
         <article className={styles.littleWhiteBlock}>
                                 <h3 className={styles.cardTitle}>{title}</h3>
                                 <p className={styles.grayTextLite}>{categories?.map((cat, index) => <span key={index}>{cat}</span>)}</p>
                                 <p className={styles.littleTextHH}>{desc}</p>
-                                <button className={styles.selectedButton} type="button">Пройти заново</button>
+                                {done ? (
+                                    <button className={styles.DoneselectedButton} type="button">Пройти заново</button>
+                                ) : (
+                                    <button className={styles.selectedButton} type="button">Пройти</button>
+                                )}
+
                             </article>
     )
 }
